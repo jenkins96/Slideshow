0002269caadb557fb318ae9e49c25c7820161f0c
jenkins96 <adrianjnkns@gmail.com>
2021-03-31 02:55:40 +0000

    slide now working

--- js/my_slide.js
+++ js/my_slide.js
@@ -4,7 +4,9 @@ let p = {
     item: 0,
     cajaSlide: document.querySelector('#slide ul'),
     animacionSlide: "fade",
-    imgSlide: document.querySelectorAll("#slide ul li")
+    imgSlide: document.querySelectorAll("#slide ul li"),
+    avanzar: document.querySelector("#slide #avanzar"),
+    retroceder: document.querySelector("#slide #retroceder")
 
 };
 
@@ -18,12 +20,33 @@ let m = {
         for (let i = 0; i < p.paginacion.length; i++) {
             p.paginacion[i].addEventListener("click", m.paginacionSlide)
         }
+        p.avanzar.addEventListener("click", m.avanzar);
+        p.retroceder.addEventListener("click", m.retroceder);
     },
     paginacionSlide(item) {
         p.item = item.target.parentNode.getAttribute('item') - 1;
         m.movimientoSlide(p.item)
 
     },
+    avanzar(){
+        
+        if(p.item == p.imgSlide.length-1){
+            p.item = 0;
+        }else {
+            p.item++;
+        }
+        
+        m.movimientoSlide(p.item);
+    },
+    retroceder(){
+        if(p.item == 0){
+            p.item = p.imgSlide.length - 1;
+        }else {
+            p.item--;
+        }
+        
+        m.movimientoSlide(p.item);
+    } ,
     movimientoSlide(item) {
         p.cajaSlide.style.left = item * -100 + '%'
         for (let i = 0; i < p.paginacion.length; i++) {
