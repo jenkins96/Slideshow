368fd202a91f1459d8aa5ff83a046b2f496f614d
jenkins96 <adrianjnkns@gmail.com>
2021-04-05 20:40:04 +0000

    message

--- js/mouse.js
+++ js/mouse.js
@@ -3,7 +3,12 @@
 =============================================*/
 
 let pm = {
-    zona: document.querySelector('#efectoMouse')
+    zona: document.querySelector('#efectoMouse'),
+    figuras: document.querySelectorAll('#efectoMouse figure'),
+    mouseX: 0,
+    mouseY: 0,
+    horizontal: true,
+    vertical: true
 }
 
 
@@ -13,12 +18,38 @@ let pm = {
 /*=============================================
         OBJETO CON LOS METODOS DEL MOUSE
 =============================================*/
-let mm ={
-    inicioMouse(){
-        pm.zona.addEventListener("mousemove", mm.movimientoMouse)
+let mm = {
+    inicioMouse() {
+        pm.zona.addEventListener("mousemove", mm.movimientoMouse);
+        for (let i = 0; i < pm.figuras.length; i++) {
+            pm.figuras[i].innerHTML = `<img src="img/mouse/plano0${i}.png">`;
+            pm.figuras[i].style.zIndex = -i
+        }
+        setTimeout(function () {
+            pm.zona.style.height = pm.figuras[0].childNodes[0].height + "px";
+
+        }, 1000)
+
+
+
     },
-    movimientoMouse(mouse){
-        console.log(mouse.offsetX, mouse.offsetY);
+    movimientoMouse(mouse) {
+        pm.mouseX = -mouse.offsetX;
+        pm.mouseY = mouse.offsetY;
+
+        for (let i = 0; i < pm.figuras.length; i++) {
+
+            if (pm.horizontal) {
+                pm.figuras[i].style.left = pm.mouseX / (i * 100 + 50) + "%";
+            }
+            if (pm.vertical) {
+                pm.figuras[i].style.top = pm.mouseY / (i * 100 + 50) + "%";
+            }
+
+
+
+        }
+
     }
 }
 
