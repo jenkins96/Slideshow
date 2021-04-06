2a24d773b1ae0b93b9c52143906bf2bfb1303c83
jenkins96 <adrianjnkns@gmail.com>
2021-04-06 19:42:45 +0000

    comment

---
+++ js/scroll.js
@@ -0,0 +1,86 @@
+/*=============================================
+        OBJETO CON LAS PROPIEDADES DEL SCROLL
+=============================================*/
+let ps ={
+    posicionScroll: 0,
+    articulos: document.querySelectorAll("#scroll article"),
+    cajaScroll: document.querySelector("#scroll"),
+    header: document.querySelector("header"),
+    botonera: document.querySelectorAll("nav ul li a"),
+    ruta: null,
+    intervalo: null,
+    destinoScroll: 0,
+    padding: 0
+
+}
+
+/*=============================================
+        OBJETO CON LOS METODOS DEL SCROLL
+=============================================*/
+let ms ={
+    inicioScroll(){
+        document.addEventListener("scroll", ms.efectoParallax)
+        for(let i = 0; i < ps.botonera.length; i++){
+            ps.botonera[i].addEventListener("click", ms.desplazamiento)
+        }
+    },
+    efectoParallax(){
+        ps.posicionScroll = window.pageYOffset;
+
+        if(ps.posicionScroll > 100){
+            ps.header.style.position = "fixed";
+            ps.header.style.zIndex = "10";
+            ps.padding = 80;
+        } else {
+            ps.header.style.position = "relative";
+            ps.header.style.zIndex = "0";
+            ps.padding = 180;
+        }
+        if(ps.posicionScroll > ps.cajaScroll.offsetTop){
+            
+            for(let i = 0; i < ps.articulos.length; i++){
+                ps.articulos[i].style.marginLeft = "0";
+            }
+            
+        } else {
+            for(let i = 0; i < ps.articulos.length; i++){
+                ps.articulos[i].style.marginLeft = ps.posicionScroll/25 - 100 + "%";
+            }
+        }
+    },
+    desplazamiento(ruta){
+        ruta.preventDefault()
+       ps.ruta =  ruta.target.getAttribute("href");
+        ps.destinoScroll = document.querySelector(ps.ruta).offsetTop - ps.padding;
+
+       ps.intervalo = setInterval(function(){
+
+           if(ps.posicionScroll < ps.destinoScroll){
+
+             ps.posicionScroll += 80;
+
+             if(ps.posicionScroll >= ps.destinoScroll){
+                 ps.posicionScroll = ps.destinoScroll;
+
+                 clearInterval(ps.intervalo);
+             }
+           } else{
+               ps.posicionScroll -= 80;
+
+             if(ps.posicionScroll <= ps.destinoScroll){
+                 ps.posicionScroll = ps.destinoScroll;
+
+                 clearInterval(ps.intervalo);
+             }
+
+           }
+       
+
+        window.scrollTo(0, ps.posicionScroll);
+
+       }, 50)
+       
+    }
+}
+
+ms.inicioScroll();
